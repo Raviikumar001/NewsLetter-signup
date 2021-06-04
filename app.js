3b356378f2dbb55cf706ae287abcf13ca12a145c
ravi <samcollins9899@gmail.com>
2021-06-04 13:41:24 +0530

    fifth change

--- app.js
+++ app.js
@@ -3,80 +3,80 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-//const request = require("request");
+const request = require("request");
 const https = require("https");
-//const { response } = require("express");
+const { response } = require("express");
 const app = express();
 
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-// app.get("/",function(req,res)
-// {
-//   res.sendFile(__dirname+"/signup.html");
-// });
+app.get("/",function(req,res)
+{
+  res.sendFile(__dirname+"/signup.html");
+});
 
-// app.post("/",function(req,res)
-// {
-//     const FirstName = req.body.fName;
-//     const LastName  = req.body.lName;
-//     const email = req.body.email;
+app.post("/",function(req,res)
+{
+    const FirstName = req.body.fName;
+    const LastName  = req.body.lName;
+    const email = req.body.email;
 
-//     var data = {
+    var data = {
 
-//         members: [
-//             {
-//                 email_address: email,
-//                 status: "subscribed",
-//                 merge_fields: {
-//                     FNAME: FirstName,
-//                     LNAME: LastName
-//                 }
-//             }
-//         ]
-//     };
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: FirstName,
+                    LNAME: LastName
+                }
+            }
+        ]
+    };
 
-//     const jsondata = JSON.stringify(data);
+    const jsondata = JSON.stringify(data);
 
-//     const url = "https://us6.api.mailchimp.com/3.0/lists/3fdd6d58c1";
-//     const options = {
-//         method: "POST",
-//         auth: "ravi1:e60151986ebbb203d9b71bd76408bc14-us6"
-//     }
+    const url = "https://us6.api.mailchimp.com/3.0/lists/3fdd6d58c1";
+    const options = {
+        method: "POST",
+        auth: "ravi1:e60151986ebbb203d9b71bd76408bc14-us6"
+    }
     
 
-//   const request=   https.request(url,options, function(response)
-// {
-//     if(response.statusCode ==  200)
-//     { 
-//         res.sendFile(__dirname+"/success.html");
-//     }else{
-//         res.sendFile(__dirname+"/failure.html"); 
-//     }
-
-//     response.on("data", function(data){
-//         console.log(JSON.parse(data));
-//     });
-// });
-
-// request.write(jsondata);
-// request.end();
-
-// });
-
-
-
-// app.post("/failure", function(req,res)
-// {
-//     res.redirect("/");
-// });
-
-
-app.get("/", function(req,res)
+  const request=   https.request(url,options, function(response)
 {
-    res.send("how ar u");
-})
+    if(response.statusCode ==  200)
+    { 
+        res.sendFile(__dirname+"/success.html");
+    }else{
+        res.sendFile(__dirname+"/failure.html"); 
+    }
+
+    response.on("data", function(data){
+        console.log(JSON.parse(data));
+    });
+});
+
+request.write(jsondata);
+request.end();
+
+});
+
+
+
+app.post("/failure", function(req,res)
+{
+    res.redirect("/");
+});
+
+
+// app.get("/", function(req,res)
+// {
+//     res.send("how ar u");
+// })
 
 app.listen(process.env.PORT || 3000, function()
 {
